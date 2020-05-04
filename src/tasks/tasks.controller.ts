93719e43037c7f22f1847bf14f7c6cac375c29f5
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-05-04 15:01:48 -0400

    add middleware to check for valid input in update task status function

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,6 +4,7 @@ import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task.model';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -31,10 +32,11 @@ export class TasksController {
     return this.tasksService.createTask(creatTaskDto);
   }
 
+  // using TaskStatusValidationPipe middleware
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
