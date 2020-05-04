f7ed7fa04f8ec23472144c9a8a6c4669572852a8
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-05-04 14:47:17 -0400

    handle task id not found scenario

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -30,7 +30,13 @@ export class TasksService {
   }
   
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const taskFound = this.tasks.find(task => task.id === id);
+
+    if (!taskFound) {
+      throw new NotFoundException(`Task not found with Id: ${id}`);
+    }
+
+    return taskFound;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
