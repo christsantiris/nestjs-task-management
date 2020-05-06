1bed80f30e616a2c3d27df49b87e7bab296c2675
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-05-06 16:25:48 -0400

    filter get tasks from postgres

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -10,7 +10,15 @@ export class TaskRepository extends Repository<Task> {
   async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
     const { status, search } = filterDto;
     
-    const query = this.createQueryBuilder();
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
+    }
 
     const tasks = query.getMany();
 
