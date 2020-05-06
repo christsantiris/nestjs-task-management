35efbfe7c88b04f555d43c851ce2a7afca72d562
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-05-06 12:49:59 -0400

    move database logic to repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -44,15 +44,7 @@ export class TasksService {
   }
 
   async createTask(createTaskDto: CreateTaskDto) {
-    const { title, description } = createTaskDto;
-
-    const task = new Task();
-    task.title = title;
-    task.description = description;
-    task.status = TaskStatus.OPEN;
-
-    await task.save();
-    return task;
+    return this.taskRepository.createTask(createTaskDto);
   }
 
   // updateTaskStatus(id: string, status: TaskStatus): Task {
