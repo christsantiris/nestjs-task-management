e4587c393e32e04308b5df4d103303fff19bc91a
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-05-12 16:16:21 -0400

    reject invalid credentials on login and log username on success

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -16,8 +16,6 @@ export class UserRepository extends Repository<User> {
     user.salt = salt;
     user.password = await this.hashPassword(password, salt);
 
-    console.log(user.password)
-
     try {
       await user.save();
     } catch (error) {
@@ -29,6 +27,17 @@ export class UserRepository extends Repository<User> {
       }
     }
     
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && await user.validatePassword(password)) {
+      return user.username;
+    } else {
+      return null;
+    }
   }
 
   private async hashPassword(password: string, salt: string): Promise<string> {
