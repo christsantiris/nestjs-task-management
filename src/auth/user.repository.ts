f04432fad3dfb51a9628151a2abdf021d12bb559
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-05-12 13:44:08 -0400

    encrypt password in db with bcrypt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,5 @@
 import { Repository, EntityRepository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
@@ -8,9 +9,14 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = salt;
+    user.password = await this.hashPassword(password, salt);
+
+    console.log(user.password)
 
     try {
       await user.save();
@@ -23,5 +29,9 @@ export class UserRepository extends Repository<User> {
       }
     }
     
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
