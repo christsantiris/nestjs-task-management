ed8ff4dc720be2a35b91cf4cb770b70ac461fe87
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-05-27 21:28:13 -0400

    refactor enum validation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -3,7 +3,7 @@ import { IsOptional, IsIn, IsNotEmpty } from "class-validator";
 
 export class GetTasksFilterDto {
   @IsOptional()
-  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+  @IsIn(Object.values(TaskStatus))
   status: TaskStatus
 
   @IsOptional()
